20cef96a824664d6c7f6258846825cc897c99a18
bijan102 <bfazeli1@gmail.com>
2016-11-11 19:26:48 -0800

    Implemented test cases for countClumps and linearIn fnx.

--- A250_Lab_12_Fazeli_Bijan/Main.cpp
+++ A250_Lab_12_Fazeli_Bijan/Main.cpp
@@ -10,6 +10,7 @@ Lab 12: STL Sets and Maps
 #include <iostream>
 #include <map>
 #include <set>
+#include <math.h>
 
 using namespace std;
 
@@ -29,7 +30,7 @@ int main()
 	multiset<int> set7 = { 1, 3, 5, 7, 7, 8, 9 };
 	multiset<int> set8 = {};
 
-	multiset<int> multiArray [] = {
+	multiset<int> multiSetArray [] = {
 		set1, set2, set3, set4, set5, set6, set7, set8
 	};
 
@@ -43,10 +44,54 @@ int main()
 		{ {3,3}, {3,3}, {3,3}, {3,4}, {4,3} }
 	};
 
-	for (int i = 0; i < 8; i++) cout << countClumps(multiArray[i]) << endl;
+	// Printing and evaluating countClumps fnx
+	multiset<int>::const_iterator it, end;
+	int temp, size;
+	cout << "Testing count clumps" << endl;
+	for (int current = 1; current <= 8; ++current)
+	{
+		// Print the current set
+		cout << "Evaluating set " << current << " { ";
+
+		temp = 1;
+		end = multiSetArray[current - 1].cend();
+		for (it = multiSetArray[current - 1].cbegin();
+			it != end; ++it)
+		{
+			size = multiSetArray[current - 1].size();
+			cout << *it <<
+				((temp != size) ? ", " : "");
+			++temp;
+		}
+		cout << " } --> " << countClumps(multiSetArray[current - 1]) << endl;
+	}
 
 	set<int> setSet = {};
 	multiples(setSet, 3);
+
+
+	// Printing and evaluating linearIn fnx
+	multimap<int, int>::const_iterator it_m_m, end_m_m;
+	cout << "\n\nTesting linear In fnx" << endl;
+	for (int current = 1; current <= 7; ++current)
+	{
+		// Print the current set
+		cout << "Evaluating set " << current << " { ";
+
+		temp = 1;
+		end_m_m = multiMapArray[current - 1].cend();
+		for (it_m_m = multiMapArray[current - 1].cbegin();
+			it_m_m != end_m_m; ++it_m_m)
+		{
+			size = multiMapArray[current - 1].size();
+			cout << "(" << it_m_m->first << ", " << it_m_m->second <<
+				((temp != size) ? "), " : ")");
+			++temp;
+		}
+		cout << " } --> " << linearIn(multiMapArray[current - 1]) << endl;
+	}
+	cout << endl;
+
 
 	set<int>::const_iterator iter;
 	for (iter = setSet.cbegin(); iter != setSet.cend(); ++iter)
